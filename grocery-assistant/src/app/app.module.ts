0517881935ed98849d3ad7509bf3802fae6021be
Patrick Soisson <32037100+PSoysauce@users.noreply.github.com>
2020-04-22 16:57:24 -0500

    Added Header and Footer with basic layout and functionality

--- grocery-assistant/src/app/app.module.ts
+++ grocery-assistant/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { VolunteerComponent } from './volunteer/volunteer.component';
+import { NeedHelpComponent } from './need-help/need-help.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    VolunteerComponent,
+    NeedHelpComponent
   ],
   imports: [
     BrowserModule,
