cec68ce07b12d871ba9e96775b4a01d89e9eeb24
Patrick Soisson <32037100+PSoysauce@users.noreply.github.com>
2020-04-21 23:13:03 -0500

    Added the volunteer component to the application

---
+++ grocery-assistant/src/app/volunteer/volunteer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-volunteer',
+  templateUrl: './volunteer.component.html',
+  styleUrls: ['./volunteer.component.sass']
+})
+export class VolunteerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
