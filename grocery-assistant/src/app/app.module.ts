d8a7f8166c5f65cc39d51236a470a51b07ab9e96
Patrick Soisson <32037100+PSoysauce@users.noreply.github.com>
2020-04-22 23:01:17 -0500

    updated app.module.ts

--- grocery-assistant/src/app/app.module.ts
+++ grocery-assistant/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +19,8 @@ import { NeedHelpComponent } from './need-help/need-help.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
     AppRoutingModule
   ],
   providers: [],
