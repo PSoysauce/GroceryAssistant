81b2830845183082c9d5921fec85d494ac56a4d8
Patrick Soisson <32037100+PSoysauce@users.noreply.github.com>
2020-04-22 22:58:11 -0500

    Updated the need-help component with the basic grocerylist function

--- grocery-assistant/src/app/need-help/need-help.component.ts
+++ grocery-assistant/src/app/need-help/need-help.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from './shared/todo.service';
 
 @Component({
   selector: 'app-need-help',
   templateUrl: './need-help.component.html',
-  styleUrls: ['./need-help.component.sass']
+  styleUrls: ['./need-help.component.sass'],
+  providers: [TodoService]
 })
 export class NeedHelpComponent implements OnInit {
-
-  constructor() { }
+  toDoListArray: any[];
+  constructor(private toDoService: TodoService) { }
 
   ngOnInit(): void {
+    this.toDoService.getToDoList().snapshotChanges()
+    .subscribe(item => {
+      this.toDoListArray = [];
+      item.forEach(element=> {
+        var x = element.payload.toJSON();
+        x["$key"] = element.key;
+        this.toDoListArray.push(x);
+      })
+
+      this.toDoListArray.sort((a,b) => {
+        return a.isChecked - b.isChecked;
+      })
+    });
   }
 
 }
