df602f7a8125fa548f602fe0e4321e3451949aa2
Patrick Soisson <32037100+PSoysauce@users.noreply.github.com>
2020-04-23 13:41:17 -0500

    added functionality to display the shopping list from the db

--- grocery-assistant/src/app/volunteer/volunteer.component.ts
+++ grocery-assistant/src/app/volunteer/volunteer.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from './shared/todo.service';
 
 @Component({
   selector: 'app-volunteer',
   templateUrl: './volunteer.component.html',
-  styleUrls: ['./volunteer.component.sass']
+  styleUrls: ['./volunteer.component.sass'],
+  providers: [TodoService]
+
 })
 export class VolunteerComponent implements OnInit {
-
-  constructor() { }
+  toDoListArray: any[];
+  constructor(private toDoService: TodoService) { }
 
   ngOnInit(): void {
+    this.toDoService.getToDoList().snapshotChanges()
+    .subscribe(item => {
+      this.toDoListArray = [];
+      item.forEach(element=> {
+        var x = element.payload.toJSON();
+        x["$key"] = element.key;
+        this.toDoListArray.push(x);
+      })
+
+      this.toDoListArray.sort((a,b) => {
+        return a.isChecked - b.isChecked;
+      })
+    });
   }
 
+  alterCheck($key: string, isChecked) {
+    this.toDoService.checkOrUnCheckTitle($key,!isChecked);
+  }
 }
