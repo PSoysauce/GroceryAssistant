6b1988fdfcef42b62b877d1d16a4fd4f4a728ef1
Patrick Soisson <32037100+PSoysauce@users.noreply.github.com>
2020-04-23 19:52:03 -0500

    added the routing guard

--- grocery-assistant/src/app/app-routing.module.ts
+++ grocery-assistant/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { NeedHelpComponent } from './need-help/need-help.component';
 import { VolunteerComponent } from './volunteer/volunteer.component';
+import { AuthGuard } from './auth.guard';
 
 
 const routes: Routes = [
   {path: 'login',component: LoginComponent},
-  {path: 'need-help', component: NeedHelpComponent},
-  {path: 'volunteer', component: VolunteerComponent},
+  {path: 'need-help', component: NeedHelpComponent, canActivate:[AuthGuard]},
+  {path: 'volunteer', component: VolunteerComponent, canActivate:[AuthGuard]},
   {path: '**', redirectTo:'login'}
 
 ];
